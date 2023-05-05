a0aab839a6cfa06b2d339656e746d64ed62601bf
swathi sb <swathi.sb16@gmail.com>
2023-05-05 22:15:41 +0530

    fix: adjusted the indentation

--- istriangle.js
+++ istriangle.js
@@ -10,14 +10,14 @@ function calculateSumOfAngle(angle1, angle2, angle3) {
 
 function isTriangle() {
     const sumOfAngles = calculateSumOfAngle(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
-        if (sumOfAngles === 180) {
-            message.innerText = "Yay ! The angles form a triangle"
-        } else {
-            message.innerText = "Oh Oh The angles don't form a triangle"
-        }
-        
-
+    if (sumOfAngles === 180) {
+        message.innerText = "Yay ! The angles form a triangle"
+    } else {
+        message.innerText = "Oh Oh The angles don't form a triangle"
     }
 
 
-    isTriangleBtn.addEventListener("click", isTriangle)
+}
+
+
+isTriangleBtn.addEventListener("click", isTriangle)
